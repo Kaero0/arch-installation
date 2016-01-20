023a97488194bd45d6234728469e586a4a8cb76a
Lajto <lajto@fastmail.com>
2016-01-20 17:58:57 +0100

    Create 04_user.sh

---
+++ 04_user.sh
@@ -0,0 +1,12 @@
+
+
+# Wifi
+#sudo nmcli dev wifi connect "SSID" password "contraseña"
+
+# Pacman configuration
+sudo echo "Color" >> /etc/pacman.conf
+sudo echo "" >> /etc/pacman.conf
+sudo echo "[multilib]" >> /etc/pacman.conf
+sudo echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+
+#
