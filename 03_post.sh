db86f28bb9dc699c03bd01f28e220fb7fcff4521
Lajto <lajto@fastmail.com>
2016-01-20 17:45:40 +0100

    Update 03_post.sh

--- 03_post.sh
+++ 03_post.sh
@@ -8,5 +8,3 @@ systemctl enable NetworkManager.service
 useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash lajto
 passwd lajto
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-
-reboot
