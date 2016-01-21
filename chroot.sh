080fe99926b285e772b1d335f4f325b80e0005f1
Lajto <lajto@fastmail.com>
2016-01-21 21:01:29 +0100

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -33,5 +33,13 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # Network
 echo "arch-pc" >> /etc/hostname
 
+# Internet
+systemctl enable NetworkManager.service
+
+# User settings (replace "lajto" with your user name)
+useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash lajto
+passwd lajto
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
 # Root password
 passwd
